f0085a321682ad7a6725c184e27ecb325aec4ad9
bendwild <dewildebenjamin0@gmail.com>
2025-06-12 16:09:21 +0200

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,9 +7,12 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <div class={classNames(displayClass, "page-title-container")}>
+      <h1 class="page-title">
+        <img src="https://github.com/bendwild/my-notes/blob/v4/content/06%20Toolkit/Files/Lovepik_com-401580942-frog.png?raw=true" alt="Logo"></img>
+        <a href={baseDir}>{title}</a>
+      </h1>
+    </div>
   )
 }
 
@@ -17,7 +20,6 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
 }
 `
 
