6d0b723b7ef16560464be16dd7f7230bcbc47bd4
bendwild <dewildebenjamin0@gmail.com>
2025-06-14 13:33:25 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "my digital swamp",
+    pageTitle: "<digital swamp>",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
